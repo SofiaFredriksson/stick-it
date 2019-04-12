aeb5ca58d943c3977ac54afffed57509c4ed1541
SofiaFredriksson <sofiapkf@gmail.com>
2019-04-12 17:27:28 +0100

    added colour to note card

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,12 +1,12 @@
-import React from 'react' 
+import React from 'react'
 
 class NoteCard extends React.Component {
 
 
   render () {
     return (
-      <div>
-          <h2>{this.props.note.title}</h2>
+      <div style={{backgroundColor: "red"}}>
+          <h2 style={{color: "pink"}}>{this.props.note.title}</h2>
                     <p>{this.props.note.content}</p>
                         </div>
     )
